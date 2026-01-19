499f468e3094019901b70588cff4d7cb0df0b1be
strwbrry05 <jq.strwbrry05@gmail.com>
2026-01-18 23:31:28 -0800

    deployment test 3 new assets file

---
+++ src/Components/Assets.jsx
@@ -0,0 +1,23 @@
+import js from "../assets/bookJs.png";
+import node from "../assets/bookNode.png";
+import css from "../assets/bookCss.png";
+import html from "../assets/bookHtml.png";
+import figma from "../assets/bookFigma.png";
+import react from "../assets/bookReact.png";
+import sql from "../assets/bookSql.png";
+import tailwind from "../assets/bookTailwind.png";
+import git from "../assets/bookGit.png";
+
+const assets = {
+  js,
+  node,
+  css,
+  html,
+  figma,
+  react,
+  sql,
+  tailwind,
+  git,
+};
+
+export default assets;
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,32 +1,44 @@
 import React from "react";
-import js from "../../public/bookJs.png";
-import node from "../assets/bookNode.png";
-import css from "../assets/bookCss.png";
-import html from "../assets/bookHtml.png";
-import figma from "../assets/bookFigma.png";
-import react from "../assets/bookReact.png";
-// import sql from "../assets/bookSql.png";
-import tailwind from "../assets/bookTailwind.png";
-import git from "../assets/bookGit.png";
+import assets from "./Assets";
 
 const Skills = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <h2>Skills & Technologies</h2>
       <div className="flex flex-wrap w-[200px] bg-blue-200 justify-center items-center">
-        <img className="w-[100px] h-[130px]" src={js} alt="javascript skill" />
-        <img className="w-[100px] h-[120px]" src={node} alt=" nodeJS skill" />
-        <img className="w-[100px] h-[130px]" src={css} alt="css skill" />
-        <img className="w-[100px] h-[130px]" src={html} alt="html skill" />
-        <img className="w-[100px] h-[130px]" src={react} alt="react skill" />
-        <img className="w-[100px] h-[130px]" src={git} alt="git skill" />
         <img
           className="w-[100px] h-[130px]"
-          src={tailwind}
+          src={assets.js}
+          alt="javascript skill"
+        />
+        <img
+          className="w-[100px] h-[120px]"
+          src={assets.node}
+          alt=" nodeJS skill"
+        />
+        <img className="w-[100px] h-[130px]" src={assets.css} alt="css skill" />
+        <img
+          className="w-[100px] h-[130px]"
+          src={assets.html}
+          alt="html skill"
+        />
+        <img
+          className="w-[100px] h-[130px]"
+          src={assets.react}
+          alt="react skill"
+        />
+        <img className="w-[100px] h-[130px]" src={assets.git} alt="git skill" />
+        <img
+          className="w-[100px] h-[130px]"
+          src={assets.tailwind}
           alt="tailwind skill"
         />
-        <img className="w-[100px] h-[130px]" src={css} alt="css skill" />
-        <img className="w-[100px] h-[130px]" src={figma} alt="figma skill" />
+        <img className="w-[100px] h-[130px]" src={assets.sql} alt="css skill" />
+        <img
+          className="w-[100px] h-[130px]"
+          src={assets.figma}
+          alt="figma skill"
+        />
       </div>
     </div>
   );
