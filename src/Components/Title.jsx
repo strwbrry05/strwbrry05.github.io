94c6d3833eec1243673fdba3e209447690b0db6f
strwbrry05 <jq.strwbrry05@gmail.com>
2026-01-18 22:01:47 -0800

    title card

---
+++ src/Components/Title.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div
+      className="font-(family-name:--font-titles) bg-(--color-white)
+    flex justify-center items-center flex-col leading-5 lg:leading-6"
+    >
+      <h1
+        className="text-[2.5rem] mt-[1em]
+      lg:text-[3rem]"
+      >
+        jacqueline molina
+      </h1>
+      <p
+        className="text-[1.5rem] mb-[1em]
+      lg:text-[2rem]"
+      >
+        your next hire.
+      </p>
+    </div>
+  );
+};
+
+export default Title;
