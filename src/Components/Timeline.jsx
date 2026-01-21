33ec8381cf28a485ba9d9b059bf41a224659b719
strwbrry05 <jq.strwbrry05@gmail.com>
2026-01-20 22:01:30 -0800

    mobile timeline

--- src/Components/Timeline.jsx
+++ src/Components/Timeline.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import TimelineCard from "./TimelineCard";
 
 const Timeline = () => {
+  const timeline03Content = (
+    <span>
+      data structures <br /> peer instructor
+    </span>
+  );
   return (
     <div className="bg-(--color-red)">
       <h2
@@ -14,14 +19,15 @@ const Timeline = () => {
       <div className="md:hidden flex justify-center items-center">
         {/* grid */}
         <div
-          className="grid grid-cols-6 grid-rows-10 w-[375px]
+          className="grid grid-cols-6 grid-rows-25 w-[375px]
         font-(family-name:--font-formal)"
         >
           {/* straight vertical line */}
           <p
-            className="w-[2px] h-[500px] bg-(--color-white)
-            row-start-1 row-end-10 col-start-1 col-end-2 justify-self-center"
+            className="w-[2px] h-[1500px] bg-(--color-white)
+            row-start-1 row-end-26 col-start-1 col-end-2 justify-self-center"
           ></p>
+          {/* 01 */}
           <TimelineCard
             hRS={0}
             hRE={3}
@@ -49,48 +55,7 @@ const Timeline = () => {
               "Operated one-on-one and small group instruction for 20+ students, indeveloping SQL comprehension and relational database skills. Assisted students with complex coursework, including helping them develop better study habits."
             }
           />
-          {/* <div
-            className={`h-[0.5px] w-[50px] ml-[2em]
-          row-start-6 row-end-6 col-start-1 col-end-3
-          border-dashed border-t-(--color-white) border-t-[2px]`}
-          ></div>
-          <div
-            className={`w-[80px] h-[80px] bg-(--color-white) rounded-[100%]
-          row-start-5 row-end-7 col-start-2 col-end-4 
-          justify-self-start self-center
-          
-          flex justify-center items-center`}
-          >
-            <p
-              className={`font-(family-name:--font-formal) font-bold text-(--color-red) text-[1.5rem]`}
-            >
-              02
-            </p>
-          </div>
-          <div
-            className={`text-(--color-white) font-bold
-          row-start-5 row-end-7 
-          col-start-3 col-end-7
-          
-          flex flex-col items-center justify-center
-          leading-5 mr-[1em]`}
-          >
-            <h3>2023-2024</h3>
-            <h3>document lead</h3>
-          </div>
-          <p
-            className={`row-start-7 row-end-10 
-        col-start-2 col-end-7
-          justify-self-center self-center
-          text-[1rem] font-(family-name:--font-funtext) text-(--color-white) leading-5 mb-[1.5em]`}
-          >
-            Managed the construction of all major documents, including user
-            manuals, and software-based documents. Interacted with developers on
-            a daily basis for design review, testing, bug review, and
-            documentation in an agile process with frequently changing
-            priorities.
-          </p> */}
-
+          {/* 02 */}
           <TimelineCard
             hRS={6}
             hRE={6}
@@ -116,6 +81,91 @@ const Timeline = () => {
             dCE={7}
             desc={
               "Managed the construction of all major documents, including usermanuals, and software-based documents. Interacted with developers on a daily basis for design review, testing, bug review, and documentation in an agile process with frequently changing priorities."
+            }
+          />
+          {/* 03 */}
+          <TimelineCard
+            hRS={11}
+            hRE={11}
+            hCS={1}
+            hCE={3}
+            hW={80}
+            cW={60}
+            cRS={10}
+            cRE={12}
+            cCS={3}
+            cCE={5}
+            cNum={"03"}
+            cNumSize={1.25}
+            tRS={10}
+            tRE={12}
+            tCS={4}
+            tCE={7}
+            tYear={"2023-2024"}
+            tTitle={timeline03Content}
+            dRS={12}
+            dRE={15}
+            dCS={2}
+            dCE={7}
+            desc={
+              "Composed engaging lesson reviews, allowing students to further their understanding of complex material, boosting motivation and lesson preparation for over 90 students. Expanded resource availability, including YouTube tutorials, handouts, practice exams, and projects."
+            }
+          />
+          {/* 04 */}
+
+          <TimelineCard
+            hRS={16}
+            hRE={16}
+            hCS={1}
+            hCE={2}
+            hW={50}
+            cW={90}
+            cRS={15}
+            cRE={17}
+            cCS={2}
+            cCE={4}
+            cNum={"04"}
+            cNumSize={1.75}
+            tRS={15}
+            tRE={17}
+            tCS={4}
+            tCE={7}
+            tYear={"2025-2026"}
+            tTitle={"freelance web desginer/developer"}
+            dRS={17}
+            dRE={20}
+            dCS={2}
+            dCE={7}
+            desc={
+              "Mastered web technologies and frameworks, allowing for the expansion of services offered to future clients. Delivered user friendly, responsive, and visually stunning websites that increase client engagement and satisfaction."
+            }
+          />
+
+          <TimelineCard
+            hRS={21}
+            hRE={21}
+            hCS={1}
+            hCE={3}
+            hW={85}
+            cW={75}
+            cRS={20}
+            cRE={22}
+            cCS={3}
+            cCE={5}
+            cNum={"05"}
+            cNumSize={1.5}
+            tRS={20}
+            tRE={22}
+            tCS={4}
+            tCE={7}
+            tYear={"2026-present"}
+            tTitle={""}
+            dRS={22}
+            dRE={25}
+            dCS={2}
+            dCE={7}
+            desc={
+              "Seeking opportunities to gain practical experience in a technology-related position. Excellent analytical and problem-solving abilities, with a passion for continuous lifelong learning. Practical experience with the software development life cycle and agile practices. Eager to contribute to team success."
             }
           />
         </div>
