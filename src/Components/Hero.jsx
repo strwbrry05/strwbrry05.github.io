f686b98e8e37deab30962992fe765b7987722f57
strwbrry05 <jq.strwbrry05@gmail.com>
2026-01-21 23:03:21 -0800

    hero functionality

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import HeroCard from "./HeroCard";
 
 import HeroMobile from "./HeroMobile";
 import HeroDesktop from "./HeroDesktop";
 
 const Hero = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div className=" flex flex-col justify-center items-center">
       {/* mobile/tablet view */}
@@ -12,11 +14,11 @@ const Hero = () => {
         className="w-[360px] flex flex-col gap-y-[1.5em]
       md:w-[750px] lg:hidden"
       >
-        <HeroMobile />
+        <HeroMobile isHovered={isHovered} setIsHovered={setIsHovered} />
       </div>
       {/* desktop+ view */}
       <div className="hidden lg:flex w-[915px] justify-center items-center">
-        <HeroDesktop />
+        <HeroDesktop isHovered={isHovered} setIsHovered={setIsHovered} />
       </div>
     </div>
   );
--- src/Components/HeroMobile.jsx
+++ src/Components/HeroMobile.jsx
@@ -7,95 +7,128 @@ import music from "../assets/heroHadestown.png";
 import hobby from "../assets/heroHobby.png";
 import art from "../assets/heroArt.png";
 import notA from "../assets/heroNotActive.png";
+import isA from "../assets/heroActive.png";
 import HeroCard from "./HeroCard";
 
-const HeroMobile = () => {
+const HeroMobile = (props) => {
   // mouse over mouse in mouse out? state var? use? to connect text and face img
   return (
     <>
       <div className="flex flex-row flex-wrap justify-center gap-y-[1em] md:gap-y-[2em]">
         {/* hobonichi */}
-        <HeroCard
-          icon={hobonichi}
-          alt={"journal"}
-          w={120}
-          title={"hobonichi"}
-          desc={
-            "my journal, diary, and time keeper. an absolute essential for me to complete and organize tasks of all callibers. i love to-do lists and scheduling my day; without my hobonichi i would be lost in the void."
-          }
-          isTitleReverse={false}
-          isReverse={false}
-        />
+        <div
+          onMouseEnter={() => props.setIsHovered(true)}
+          onMouseLeave={() => props.setIsHovered(false)}
+        >
+          <HeroCard
+            icon={hobonichi}
+            alt={"journal"}
+            w={120}
+            title={"hobonichi"}
+            desc={
+              "my journal, diary, and time keeper. an absolute essential for me to complete and organize tasks of all callibers. i love to-do lists and scheduling my day; without my hobonichi i would be lost in the void."
+            }
+            isTitleReverse={false}
+            isReverse={false}
+          />
+        </div>
 
         {/* pan */}
-        <HeroCard
-          icon={pan}
-          alt={"stuffed animal"}
-          w={80}
-          title={"pan(talaimon)"}
-          desc={
-            "stuffed animals provide a sense of comfort and nostalgia. they remind me to remain whimsical in a society that attempts to rob you of it. i keep many stuffed animals but this $5 christmas charm CHARMED me more than i thought possible. i lost him once and i wont do it again."
-          }
-          isTitleReverse={false}
-          isReverse={true}
-        />
+        <div
+          onMouseEnter={() => props.setIsHovered(true)}
+          onMouseLeave={() => props.setIsHovered(false)}
+        >
+          <HeroCard
+            icon={pan}
+            alt={"stuffed animal"}
+            w={80}
+            title={"pan(talaimon)"}
+            desc={
+              "stuffed animals provide a sense of comfort and nostalgia. they remind me to remain whimsical in a society that attempts to rob you of it. i keep many stuffed animals but this $5 christmas charm CHARMED me more than i thought possible. i lost him once and i wont do it again."
+            }
+            isTitleReverse={false}
+            isReverse={true}
+          />
+        </div>
 
         {/* book */}
-        <HeroCard
-          icon={book}
-          alt={"book: the secret history by donna tart"}
-          w={80}
-          title={"the secret history"}
-          desc={
-            "i absolutely ADORE reading. i believe individuals should all read for fun well into adulthood. as for one of my favorites, im not one to dwell on contemporary classics, but something about this novel captivated me from the first chapter. my pitch: dark academia, a friend group admist a murder, and unreliable narration."
-          }
-          isTitleReverse={false}
-          isReverse={false}
-        />
+        <div
+          onMouseEnter={() => props.setIsHovered(true)}
+          onMouseLeave={() => props.setIsHovered(false)}
+        >
+          <HeroCard
+            icon={book}
+            alt={"book: the secret history by donna tart"}
+            w={80}
+            title={"the secret history"}
+            desc={
+              "i absolutely ADORE reading. i believe individuals should all read for fun well into adulthood. as for one of my favorites, im not one to dwell on contemporary classics, but something about this novel captivated me from the first chapter. my pitch: dark academia, a friend group admist a murder, and unreliable narration."
+            }
+            isTitleReverse={false}
+            isReverse={false}
+          />
+        </div>
 
         {/* movie */}
-        <HeroCard
-          icon={movie}
-          alt={"film: baby assassins 3"}
-          w={80}
-          title={"baby assassins 3"}
-          desc={
-            "what is a person without their favorite film? two teenage assassins do their jobs and get into mischief. the future is not always guaranteed, so you might as well be confident in what you want. but always remember to enjoy the small moments in life as well AND to never take anything too seriously."
-          }
-          isTitleReverse={false}
-          isReverse={true}
-        />
+        <div
+          onMouseEnter={() => props.setIsHovered(true)}
+          onMouseLeave={() => props.setIsHovered(false)}
+        >
+          <HeroCard
+            icon={movie}
+            alt={"film: baby assassins 3"}
+            w={80}
+            title={"baby assassins 3"}
+            desc={
+              "what is a person without their favorite film? two teenage assassins do their jobs and get into mischief. the future is not always guaranteed, so you might as well be confident in what you want. but always remember to enjoy the small moments in life as well AND to never take anything too seriously."
+            }
+            isTitleReverse={false}
+            isReverse={true}
+          />
+        </div>
 
         {/* music */}
-        <HeroCard
-          icon={music}
-          alt={"broadway show: hadestown"}
-          w={80}
-          title={"hadestown"}
-          desc={
-            "the tale of orpheus and eurydice. hades and persephone. greek mythology has always been fascinating. little 18 year old me was captivated from her first listen. to know a tragic cycle will repeat and STILL hope for a better outcome every time is heartbreaking. hope will forever be everlasting. other favorites include, Cabaret and Next To Normal."
-          }
-          isTitleReverse={false}
-          isReverse={false}
-        />
+        <div
+          onMouseEnter={() => props.setIsHovered(true)}
+          onMouseLeave={() => props.setIsHovered(false)}
+        >
+          <HeroCard
+            icon={music}
+            alt={"broadway show: hadestown"}
+            w={80}
+            title={"hadestown"}
+            desc={
+              "the tale of orpheus and eurydice. hades and persephone. greek mythology has always been fascinating. little 18 year old me was captivated from her first listen. to know a tragic cycle will repeat and STILL hope for a better outcome every time is heartbreaking. hope will forever be everlasting. other favorites include, Cabaret and Next To Normal."
+            }
+            isTitleReverse={false}
+            isReverse={false}
+          />
+        </div>
 
         {/* hobby */}
-        <HeroCard
-          icon={hobby}
-          alt={"hobby: baking"}
-          w={120}
-          title={"hobby"}
-          desc={
-            "as a creative individual, i feel as though you can tap into that creativity in any field; and that includes cooking and baking! admittedly this is a new hobby, but one that has brought myself, and others around me, immense fulfillment and joy."
-          }
-          isTitleReverse={false}
-          isReverse={true}
-        />
+        <div
+          onMouseEnter={() => props.setIsHovered(true)}
+          onMouseLeave={() => props.setIsHovered(false)}
+        >
+          <HeroCard
+            icon={hobby}
+            alt={"hobby: baking"}
+            w={120}
+            title={"hobby"}
+            desc={
+              "as a creative individual, i feel as though you can tap into that creativity in any field; and that includes cooking and baking! admittedly this is a new hobby, but one that has brought myself, and others around me, immense fulfillment and joy."
+            }
+            isTitleReverse={false}
+            isReverse={true}
+          />
+        </div>
       </div>
       {/* art */}
       <div
         className={`font-(family-name:--font-text) text-(--color-gray) flex flex-col justify-center items-center
-            cursor-pointer hover:text-black`}
+           cursor-default hover:text-black`}
+        onMouseEnter={() => props.setIsHovered(true)}
+        onMouseLeave={() => props.setIsHovered(false)}
       >
         <div className={`flex flex-col justify-center items-center`}>
           <p className="text-[1.15rem] text-center mb-[0.5em]">edward hopper</p>
@@ -112,7 +145,11 @@ const HeroMobile = () => {
         </div>
       </div>
       {/* self */}
-      <img src={notA} alt="" className="w-[full] md:w-[300px] self-center" />
+      {props.isHovered ? (
+        <img src={isA} alt="" className="w-[full] md:w-[300px] self-center" />
+      ) : (
+        <img src={notA} alt="" className="w-[full] md:w-[300px] self-center" />
+      )}
     </>
   );
 };
