26c31e5ac65f3112dc88ec581a7a68d184c73e71
strwbrry05 <jq.strwbrry05@gmail.com>
2026-01-21 21:41:13 -0800

    mobile hero base

--- src/Components/HeroCard.jsx
+++ src/Components/HeroCard.jsx
@@ -4,15 +4,15 @@ const HeroCard = (props) => {
   return (
     <div
       className={`font-(family-name:--font-text) text-(--color-gray)
-    ${props.isReverse ? "flex-row-reverse" : ""} flex`}
+    ${props.isReverse ? "flex-row-reverse" : ""} flex `}
     >
       <div
         className={` ${props.isTitleReverse ? "flex-col-reverse" : "flex-col"} flex  justify-center items-center w-[30%]`}
       >
-        <p className="text-[1.15rem]">hobonichi</p>
+        <p className="text-[1.15rem] text-center mb-[0.5em]">{props.title}</p>
         <img className={`w-[${props.w}px]`} src={props.icon} alt={props.alt} />
       </div>
-      <div className="w-[60%]">
+      <div className="w-[65%]">
         <p>{props.desc}</p>
       </div>
     </div>
