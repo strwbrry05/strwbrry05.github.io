e9007b99d16739e512e71c38f061a5d300b96aa4
strwbrry05 <jq.strwbrry05@gmail.com>
2026-01-29 21:20:32 -0800

    projects slides tentative complete

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -10,6 +10,10 @@ import { FaGithub } from "react-icons/fa";
 import BA from "../assets/projects_BA.png";
 import NCT from "../assets/projects_nct.png";
 import TOKYO from "../assets/projects_tokyo.png";
+import FISH from "../assets/projects_fish.png";
+import TIKTOK from "../assets/projects_tiktok.png";
+import NOTES from "../assets/projects_notes.png";
+import BASKET from "../assets/projects_basket.png";
 
 const Projects = () => {
   const slidesInfo = [
@@ -33,48 +37,48 @@ const Projects = () => {
     },
     {
       image: TOKYO,
-      alt: "kpop group nct dream showcase react app",
-      title: "NCT Dream Showcase",
+      alt: "tokyo travel itinerary react app",
+      title: "Travel To Tokyo",
       status: "completed",
-      desc: "[2025] React web app showcasing kpop group NCT Dream. Inlcudes many features such as 'day-and-night' mode, slideshows, carousels, drag-and-drop components.",
-      websiteLink: "https://nctdream-showcase.netlify.app/",
-      githubLink: "https://github.com/strwbrry05/nctDreamShowcase",
+      desc: "[2025] React web app portraying an initerary page in a fun/creative format. Inspired by old japanese magazines, the layout emulates a similar style, combining collaging/crafting methods with web development and design.",
+      websiteLink: "https://traveltotokyo.netlify.app/",
+      githubLink: "https://github.com/strwbrry05/travelToTokyo",
     },
     {
-      image: NCT,
-      alt: "kpop group nct dream showcase react app",
-      title: "NCT Dream Showcase",
+      image: FISH,
+      alt: "fake fish wholesale company",
+      title: "We Sell Fish",
       status: "completed",
-      desc: "[2025] React web app showcasing kpop group NCT Dream. Inlcudes many features such as 'day-and-night' mode, slideshows, carousels, drag-and-drop components.",
-      websiteLink: "https://nctdream-showcase.netlify.app/",
-      githubLink: "https://github.com/strwbrry05/nctDreamShowcase",
+      desc: "[2025] Web app mock-up wmulating a wholesale company website. A simple yet effective design, this site demonstrates understanding of web development principles, design aesthetics, and React coding essentials.",
+      websiteLink: "https://wesellfish.netlify.app/",
+      githubLink: "https://github.com/strwbrry05/weSellFish",
     },
     {
-      image: NCT,
-      alt: "kpop group nct dream showcase react app",
-      title: "NCT Dream Showcase",
+      image: TIKTOK,
+      alt: "compilation of categorized tiktok videos",
+      title: "TTWDFTH",
       status: "completed",
-      desc: "[2025] React web app showcasing kpop group NCT Dream. Inlcudes many features such as 'day-and-night' mode, slideshows, carousels, drag-and-drop components.",
-      websiteLink: "https://nctdream-showcase.netlify.app/",
-      githubLink: "https://github.com/strwbrry05/nctDreamShowcase",
+      desc: "[2025] A web focused page (HTML/CSS/JS) dedicated to archiving the creator's favorite tiktoks in leiu of the tiktok shutdown of 2025. Each category contains its own set of videos along with functional controls for moving between and pausing said videos. Page is responsive.",
+      websiteLink: "https://ttwdfth.netlify.app/",
+      githubLink: "https://github.com/strwbrry05/ttwdfth",
     },
     {
-      image: NCT,
-      alt: "kpop group nct dream showcase react app",
-      title: "NCT Dream Showcase",
+      image: NOTES,
+      alt: "HTML/CSS website for frontend development",
+      title: "gatit0 notes",
       status: "completed",
-      desc: "[2025] React web app showcasing kpop group NCT Dream. Inlcudes many features such as 'day-and-night' mode, slideshows, carousels, drag-and-drop components.",
-      websiteLink: "https://nctdream-showcase.netlify.app/",
-      githubLink: "https://github.com/strwbrry05/nctDreamShowcase",
+      desc: "[2024] Created initially as a fun side project in order to inspire and aid others in beginning their Frontend Web Developer journey. Designed to look like a desktop login screen; this was meant as a unconventional, yet creative, approach to 'boring' do-it-yourself tutorial websites.",
+      websiteLink: "https://gatit0notes.netlify.app/",
+      githubLink: "https://github.com/strwbrry05/gatit0Notes",
     },
     {
-      image: NCT,
-      alt: "kpop group nct dream showcase react app",
-      title: "NCT Dream Showcase",
+      image: BASKET,
+      alt: "personalized wishlist HTML/CSS",
+      title: "Berry Basket",
       status: "completed",
-      desc: "[2025] React web app showcasing kpop group NCT Dream. Inlcudes many features such as 'day-and-night' mode, slideshows, carousels, drag-and-drop components.",
-      websiteLink: "https://nctdream-showcase.netlify.app/",
-      githubLink: "https://github.com/strwbrry05/nctDreamShowcase",
+      desc: "[2024] Website created with HTML/CSS in order to demonstrate individual's understanding of web design principles. Meant to showcase a wishlist in web page format. A fun way to introduce a person's wants rather than using a typical list format.",
+      websiteLink: "https://strwbrry05-berrybasket.netlify.app/",
+      githubLink: "https://github.com/strwbrry05/berryBasket",
     },
   ];
 
@@ -83,12 +87,12 @@ const Projects = () => {
       <div className="flex justify-center">
         <h2
           className="font-(family-name:--font-titles) text-[2.5rem] ml-[1em] mt-[1em] mb-[0.5em]
-        md:ml-[7em] lg:ml-[12em]"
+        md:ml-[6em] lg:ml-[8em] md:text-[3rem] lg:text-[3.5rem]"
         >
           projects ive made
         </h2>
       </div>
-      <div className="md:hidden">
+      <div className="lg:hidden">
         <Swiper
           slidesPerView={1}
           spaceBetween={10}
@@ -96,10 +100,10 @@ const Projects = () => {
           pagination={{ clickable: true }}
           navigation={true}
           modules={[Pagination, Navigation]}
-          className="mySwiper w-[380px] h-[550px]"
+          className="mySwiper w-[380px] h-[550px] md:w-[600px]"
         >
           {slidesInfo.map((slide) => (
-            <SwiperSlide className="flex justify-center items-center text-center pl-[3em] pr-[3em]">
+            <SwiperSlide className="flex justify-center items-center text-center pl-[3em] pr-[3em] md:pl-[6em] md:pr-[3em]">
               <div className="flex justify-center items-center">
                 {/* image */}
                 <div className="w-[50%] ">
@@ -112,17 +116,19 @@ const Projects = () => {
                 {/* info side */}
                 <div className="w-[60%] text-left font-(family-name:--font-funtext) p-[1em]">
                   <div className="mb-[1em]">
-                    <h3 className="font-bold text-[1.05rem]">{slide.title}</h3>
+                    <h3 className="font-bold text-[1.05rem] md:text-[1.25rem]">
+                      {slide.title}
+                    </h3>
                     <p>status: {slide.status}</p>
                   </div>
-                  <p className="text-[0.9rem]">{slide.desc}</p>
-                  <div>
+                  <p className="text-[0.9rem] md:text-[1rem]">{slide.desc}</p>
+                  <div className="md:text-[1.05rem]">
                     <div className="flex justify-start items-center mt-[1.5em]">
                       <FaExternalLinkAlt />
                       <a
                         href={slide.websiteLink}
                         target="_blank"
-                        className="ml-[0.5em] underline cursor-pointer"
+                        className="ml-[0.5em] underline cursor-pointer hover:text-(--color-gray)"
                       >
                         website link
                       </a>
@@ -132,7 +138,7 @@ const Projects = () => {
                       <a
                         href={slide.githubLink}
                         target="_blank"
-                        className="ml-[0.5em] underline cursor-pointer"
+                        className="ml-[0.5em] underline cursor-pointer hover:text-(--color-gray)"
                       >
                         github link
                       </a>
@@ -145,7 +151,7 @@ const Projects = () => {
         </Swiper>
       </div>
 
-      <div className="hidden md:block">
+      <div className="hidden lg:block">
         <Swiper
           slidesPerView={2}
           spaceBetween={10}
@@ -153,56 +159,54 @@ const Projects = () => {
           pagination={{ clickable: true }}
           navigation={true}
           modules={[Pagination, Navigation]}
-          className="mySwiper md:w-[700px] lg:w-[950px] h-[500px]"
+          className="mySwiper lg:w-[960px] h-[600px]"
         >
-          <SwiperSlide className="flex justify-center items-center text-center">
-            <div className="flex justify-center items-center">
-              {/* image */}
-              <div className="p-[1em]">
-                <img
-                  className="w-[150px]"
-                  src={BA}
-                  alt="Baby assassins react web project"
-                />
-              </div>
-              {/* info side */}
-              <div className="w-[210px] text-left font-(family-name:--font-funtext) m-[1em] p-[1em]">
-                <div className="mb-[1em]">
-                  <h3 className="font-bold text-[1.05rem]">
-                    Baby Assassins Showcase
-                  </h3>
-                  <p>status: completed</p>
+          {slidesInfo.map((slide) => (
+            <SwiperSlide className="flex justify-center items-center text-center pl-[4em] pr-[3em]">
+              <div className="flex justify-center items-center">
+                {/* image */}
+                <div className="w-[50%] pt-[2em]">
+                  <img
+                    className="w-[100%] h-[500px]"
+                    src={slide.image}
+                    alt={slide.alt}
+                  />
                 </div>
-                <p className="text-[0.9rem]">
-                  [2025] Extremely creative React web app showcasing the
-                  Japanese movie trilogy - Baby Assassins. Web app includes a
-                  call to the OMDB api, various slideshows, selectable viewing
-                  options, gallery filters, in-page movie trailers, etc.
-                </p>
-                <div>
-                  <div className="flex justify-start items-center mt-[1.5em]">
-                    <FaExternalLinkAlt />
-                    <a href="" className="ml-[0.5em] underline cursor-pointer">
-                      website link
-                    </a>
+                {/* info side */}
+                <div className="w-[60%] text-left font-(family-name:--font-funtext) pl-[1em]">
+                  <div className="mb-[1em]">
+                    <h3 className="font-bold text-[1.5rem] leading-5">
+                      {slide.title}
+                    </h3>
+                    <p>status: {slide.status}</p>
                   </div>
-                  <div className="flex justify-start items-center">
-                    <FaGithub />
-                    <a href="" className="ml-[0.5em] underline cursor-pointer">
-                      github link
-                    </a>
+                  <p className="text-[1rem]">{slide.desc}</p>
+                  <div className="text-[1.15rem]">
+                    <div className="flex justify-start items-center mt-[1.5em]">
+                      <FaExternalLinkAlt />
+                      <a
+                        href={slide.websiteLink}
+                        target="_blank"
+                        className="ml-[0.5em] underline cursor-pointer hover:text-(--color-gray)"
+                      >
+                        website link
+                      </a>
+                    </div>
+                    <div className="flex justify-start items-center">
+                      <FaGithub />
+                      <a
+                        href={slide.githubLink}
+                        target="_blank"
+                        className="ml-[0.5em] underline cursor-pointer hover:text-(--color-gray)"
+                      >
+                        github link
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="bg-red-300">Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
